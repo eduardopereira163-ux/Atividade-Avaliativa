54cb9b9da211db70ed7774d059fc6f916410c133
Eduardo Pereira <eduardo_pereira163@estudante.sesisenai.org.br>
2026-09-10 20:36:58 -0300

    mudei a cor das mensagens de erro

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ form.addEventListener("submit", (event) => {
   const cidade = document.getElementById("cidade") 
     .value.trim().replace(/\s+/g, " "); 
   mensagem.textContent = ""; 
-  
-  if (!/^\d{5}-?\d{3}$/.test(entrada)) { 
-    mensagem.textContent = "Informe um CEP com 8 números."; 
+  mensagem.classList.remove("erro");
+
+ if (!/^\d{5}-?\d{3}$/.test(entrada)) { 
+    mensagem.textContent = "Informe um CEP com 8 números.";
+    mensagem.classList.add("erro");
     return; 
-  } 
+}
   if (!cidade) { 
-    mensagem.textContent = "Informe a cidade."; 
+    mensagem.textContent = "Informe a cidade.";
+    mensagem.classList.add("erro");
     return; 
-  } 
+}
   const cep = entrada.replace("-", ""); 
   const parametros = new URLSearchParams({ cep, cidade }); 
   window.location.href = `resultado.html?${parametros}`; 
